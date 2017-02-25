d061982fe175c34f4ef445361e8b6fa4fc677636
Bartlomiej Karaban <bk@Ula-i-Bartek.home>
2017-02-25 19:34:11 +0100

    automat do liczenia progu cut off dla score

--- temp.R
+++ temp.R
@@ -1,5 +1,7 @@
 
-score<-seq(0,1,0.01)
+score<-seq(0.01,1,0.01) ## sekwencja score co 1 pkt
+
+summary(data2cutoff)
 
 df.tresholds<-data.frame(treshold=score,
                TP=0,
@@ -10,7 +12,16 @@ df.tresholds<-data.frame(treshold=score,
                Gain=0
                )
 
-for i in (score) {
-  
-}
+####################################
+data2cutoff$pred.cut<-0
 
+for (i in score) {
+s<-i  
+
+for (j in 1: ncol(data2cutoff)) {
+  data2cutoff$pred.cut<-ifelse(data2cutoff$predicted.train<s,0,1)
+}
+conf.matrix<-table(data2cutoff$real, data2cutoff$pred.cut)
+df.tresholds[df.tresholds$treshold==i,"ACC"]<-sum(diag(conf.matrix))/sum(conf.matrix)
+
+}
