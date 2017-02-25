ad042aa1293190f19e28c1e4040776e33ee8156a
Bartlomiej Karaban <bk@Ula-i-Bartek.home>
2017-02-25 11:34:37 +0100

    skrypt z funkcja zysku dla odciecia score'u

---
+++ temp.R
@@ -0,0 +1,16 @@
+
+score<-seq(0,1,0.01)
+
+df.tresholds<-data.frame(treshold=score,
+               TP=0,
+               FP=0,
+               TN=0,
+               FN=0,
+               ACC=0,
+               Gain=0
+               )
+
+for i in (score) {
+  
+}
+
