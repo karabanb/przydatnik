886f506525080d57478e9ce611f0d60768659ce8
Bartlomiej Karaban <bk@Ula-i-Bartek.home>
2017-02-26 14:53:06 +0100

    dodanie wykresu dla cut off

--- temp.R
+++ temp.R
@@ -1,5 +1,8 @@
 
-score<-seq(0.01,1,0.01) ## sekwencja score co 1 pkt
+library(ggplot2)
+
+score<-round(seq(min(data2cutoff$predicted.train),max(data2cutoff$predicted.train),0.01),2) ## sekwencja score co 1 pkt
+score<-seq(0.05,0.99,0.01)
 
 summary(data2cutoff)
 
@@ -14,14 +17,26 @@ df.tresholds<-data.frame(treshold=score,
 
 ####################################
 data2cutoff$pred.cut<-0
+conf.matrix<-matrix(0,2,2)
 
-for (i in score) {
+LGD<-0.49
+profit<-0.5
+for (i in score ){
 s<-i  
 
 for (j in 1: ncol(data2cutoff)) {
   data2cutoff$pred.cut<-ifelse(data2cutoff$predicted.train<s,0,1)
 }
+conf.matrix<-matrix(0,2,2)
 conf.matrix<-table(data2cutoff$real, data2cutoff$pred.cut)
 df.tresholds[df.tresholds$treshold==i,"ACC"]<-sum(diag(conf.matrix))/sum(conf.matrix)
-
+df.tresholds[df.tresholds$treshold==i,"TP"]<-conf.matrix[2,2]
+df.tresholds[df.tresholds$treshold==i,"TN"]<-conf.matrix[1,1]
+df.tresholds[df.tresholds$treshold==i,"FP"]<-conf.matrix[1,2]
+df.tresholds[df.tresholds$treshold==i,"FN"]<-conf.matrix[2,1]
+df.tresholds[df.tresholds$treshold==i,"Gain"]<-(conf.matrix[2,2]*3000*profit)-(conf.matrix[1,2]*3000*LGD)
 }
+
+(cut.point<-df.tresholds[which.max(df.tresholds$Gain),"treshold"])
+
+ggplot(data=df.tresholds, aes(x=treshold, y=Gain))+geom_line()+geom_vline(xintercept = cut.point)
