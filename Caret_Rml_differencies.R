a01d7052792a45511e6af85f099e3ddfb958919e
karabanb <b.karaban@me.com>
2017-09-22 21:30:05 +0200

    Dodanie do repo, model w caret, confusion matrix i ROC

---
+++ Caret_Rml_differencies.R
@@ -0,0 +1,45 @@
+
+#### loading required libraries #######
+
+library(caret)
+library(mlr)
+library(dplyr)
+library(ggplot2)
+library(rpart)
+library(ROCR)
+library(pROC)
+
+
+
+##### loading daset ####################
+
+data("iris")
+names(iris)
+attach(iris)
+
+raw.data <- dplyr::filter(iris, Species %in% c("virginica", "versicolor"))
+raw.data$Species <- droplevels(raw.data$Species)
+
+
+###### building the models  ############
+
+fitControl <- trainControl(method = "boot",
+                           number = 100,
+                           repeats = 100,
+                           classProbs = TRUE,
+                           summaryFunction = twoClassSummary)
+
+m1.0 <- caret::train(Species~.,
+                     data = raw.data,
+                     method = "rpart",
+                     trControl = fitControl,
+                     metric = "ROC")
+ 
+m1.0_pred <- predict(m1.0, newdata = raw.data, type = "raw")
+
+confusionMatrix(data = m1.0_pred, reference =  raw.data$Species, positive = "virginica")
+
+m1.0_pred <- ifelse(m1.0_pred %in% "virginica",1,0)
+
+plot.roc(roc(response = raw.data$Species, predictor = m1.0_pred))
+ 
