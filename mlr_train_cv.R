38fb3931c0203000351216cc59f44cb7991e62e4
karabanb <b.karaban@me.com>
2017-10-15 15:13:42 +0200

    dodanie modeli

--- mlr_train_cv.R
+++ mlr_train_cv.R
@@ -1,6 +1,9 @@
 
 library(mlr)
 library(caret)
+library(rpart)
+library(rpart.plot)
+library(ggplot2)
 
 data("GermanCredit")
 
@@ -10,9 +13,50 @@ classif.task <- makeClassifTask(data = GermanCredit, target = "Class")
 
 classif.lrn <-makeLearner("classif.rpart", predict.type = "prob")
 
+#### modyfing of the hyperparameteres #######
+
+classif.lrn <- setHyperPars(classif.lrn, maxdepth =3)
+
+m1 <- mlr::train(classif.lrn, classif.task)
+
+m1.rpart <- getLearnerModel(m1)   
+
+rpart.plot(m1.rpart)
+
 ## definiovanie CV
 
-rdesc <- makeResampleDesc("CV",predict = "test")
+rdesc <- makeResampleDesc("CV",predict = "test", iters = 10)
 
-r <- resample(learner = classif.lrn,task = classif.task, resampling = rdesc, measures =  list(auc,acc))
+r <- resample(
+                learner = classif.lrn
+              , task = classif.task
+              , resampling = rdesc
+              , measures =  list(auc,acc, fpr, tnr)
+              , models = TRUE
+              )
+
+## wyniki CV
+
+CVScore <- r$measures.test
+
+summary(r$measures.test[,-1])
+
+barsAUC <- ggplot(data = CVScore, aes(iter, auc)) +
+           geom_col() +
+           geom_line( aes(iter, mean(auc)))
+
+barsACC <- ggplot(data = CVScore, aes(iter, acc)) +
+           geom_col() +
+           geom_line( aes(iter, mean(acc)))
+
+## modele
+
+modelsList <- lapply(r$models, getLearnerModel)
+
+sapply(modelsList, rpart.plot)
+
+
+
+
+
 
